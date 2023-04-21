a8993b98c9e640b177817c969869669147fd7de8
王琦 <1099046071@qq.com>
2023-04-21 21:23:23 +0800

    Update index.vue

--- apps/kubegems/src/views/authentication/index.vue
+++ apps/kubegems/src/views/authentication/index.vue
@@ -100,61 +100,7 @@
                   @keyup.enter="login(source)"
                 />
 
-                <v-menu
-                  v-model="localeMenu"
-                  attach="#login_locale"
-                  bottom
-                  left
-                  nudge-bottom="5px"
-                  offset-y
-                  origin="top center"
-                  transition="scale-transition"
-                >
-                  <template #activator="{ on }">
-                    <div id="login_locale" />
-                    <v-btn class="primary--text font-weight-medium mb-3" color="white" dark depressed small v-on="on">
-                      <img class="locale__icon mr-2" :src="`/icon/i18n/${Locale}.svg`" />
-                      {{ localeShow }}
-                      <v-icon v-if="localeMenu" right> mdi-chevron-up </v-icon>
-                      <v-icon v-else right> mdi-chevron-down </v-icon>
-                    </v-btn>
-                  </template>
-                  <v-data-iterator hide-default-footer :items="[{ text: $t('tip.i18n'), values: locales }]">
-                    <template #no-data>
-                      <v-card>
-                        <v-card-text>{{ $root.$t('data.no_data') }} </v-card-text>
-                      </v-card>
-                    </template>
-                    <template #default="props">
-                      <v-card v-for="item in props.items" :key="item.text" flat min-width="100px">
-                        <v-list dense>
-                          <v-flex class="text-subtitle-2 text-center ma-2">
-                            <span>{{ $t('tip.i18n') }}</span>
-                          </v-flex>
-                          <v-divider class="mx-2" />
-                          <v-list-item
-                            v-for="(ln, index) in item.values"
-                            :key="index"
-                            class="text-caption text-start font-weight-medium mx-2"
-                            link
-                            :style="{ color: ln.locale === locale ? `#1e88e5 !important` : `` }"
-                            @click="setLocale(ln)"
-                          >
-                            <v-list-item-content>
-                              <div>
-                                <div class="float-left mr-2">
-                                  <img class="locale__icon" :src="`/icon/i18n/${ln.locale}.svg`" />
-                                </div>
-                                <div class="float-left locale">{{ ln.title }}</div>
-                                <div class="kubegems__clear-float" />
-                              </div>
-                            </v-list-item-content>
-                          </v-list-item>
-                        </v-list>
-                      </v-card>
-                    </template>
-                  </v-data-iterator>
-                </v-menu>
+                
 
                 <v-btn
                   block
